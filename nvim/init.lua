d10625a334c05a8ca36a465499fd6c99b6bd9b90
toby <toby.chen1337@outlook.com>
2025-10-11 19:32:19 +0100

    update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,20 +15,38 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-neo-tree/neo-tree.nvim", dependencies = { "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim", } },
+}
+
+local opts = {}
+
 -- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+require("lazy").setup(plugins, opts) 
+
+local config = require("nvim-treesitter.configs")
+
+config.setup({
+    ensure_installed = {"lua", "python", "typescript", "java", "cpp", "html", "css"},
+    highlight = { enable = true},
+    indent = { enable = true},
 })
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+local builtin = require("telescope.builtin")
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<C-g>", builtin.live_grep, {})
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left <CR>")
