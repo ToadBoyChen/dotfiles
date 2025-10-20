16b7f40601fb66b647a133fbc34c396b19ced784
toby <toby.chen1337@outlook.com>
2025-10-20 16:48:28 +0100

    Polish launch script to look prettier

--- hypr/scripts/launch/clean-system.sh
+++ hypr/scripts/launch/clean-system.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash 
 
-sudo pacman -Sc --noconfirm 
-yay -Sc --noconfirm 
-rm -rf /tmp/*
-rm -rf ~/.cache/thumbnails/*
-rm -rf ~/.local/share/Trash/*
+echo "Run System Cleaner? (y/n)"
+read yes_no_clean
 
+yes_no_clean="${yes_no_clean//$'\r'/}"
+
+if [ "$yes_no_clean" = "y" ] || [ "$yes_no_clean" = "Y" ]; then
+    sudo pacman -Sc --noconfirm 
+    yay -Sc --noconfirm 
+    rm -rf /tmp/*
+    rm -rf ~/.cache/thumbnails/*
+    rm -rf ~/.local/share/Trash/*
+    sudo pacman -Rns $(pacman -Qdtq)
+    sudo journalctl --vacuum-time=3d
+else
+    echo "Skipping System Clean"
+fi 
 
--- hypr/scripts/launch/launch.sh
+++ hypr/scripts/launch/launch.sh
@@ -2,13 +2,26 @@
 
 set -e
 
-run_launch=1
+run_launch=0
 
-while [ "$run_launch" = 1 ];
+while [ "$run_launch" != -1 ];
 do
+    let run_launch+=1
+    clear
     ./title-screen.sh
-    ./update-system.sh
-    ./clean-system.sh 
 
-    run_launch=0
+    echo ""
+    echo "Stage $run_launch of 2"
+
+    if [ "$run_launch" = 1 ]; then
+        ./update-system.sh
+    elif [ "$run_launch" = 2 ]; then 
+        ./clean-system.sh 
+    else 
+        run_launch=-1
+    fi
 done
+
+clear
+echo "Launch Script Complete"
+echo "Goodbye $USER!"
