b6a003113cb7884d8207ae64f325c8cf0ff2f73e
toby <toby.chen1337@outlook.com>
2025-10-21 15:26:23 +0100

    fix lsp config and tidy up horizontal config for waybar

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,4 +1,6 @@
+-- plugins/lsp.lua
 return {
+  -- Mason for managing external tools
   {
     "williamboman/mason.nvim",
     config = function()
@@ -6,60 +8,92 @@ return {
     end,
   },
 
+  -- Mason + LSP integration
   {
     "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "clangd",
-          "ts_ls",
-          "bashls",
+          "lua_ls",    -- Lua
+          "bashls",    -- Bash
+          "pyright",   -- Python
+          "clangd",    -- C/C++
+          "ts_ls",     -- TypeScript/JavaScript
         },
       })
     end,
   },
 
+  -- LSP configuration
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" },
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Use the new Neovim 0.11+ API
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = { globals = { "vim" } },
-          },
-        },
-      })
+      -- Setup LSP servers using new vim.lsp.config API
+      local servers = { "lua_ls", "bashls", "pyright", "clangd", "ts_ls" }
+      for _, lsp in ipairs(servers) do
+        vim.lsp.config(lsp, { capabilities = capabilities })
+      end
 
-      vim.lsp.config("bashls", { capabilities = capabilities })
-      vim.lsp.config("pyright", { capabilities = capabilities })
-      vim.lsp.config("clangd", { capabilities = capabilities })
-      vim.lsp.config("ts_ls", { capabilities = capabilities })
-
-      -- Start configured LSPs automatically
-      vim.lsp.start({
-        name = "lua_ls",
-        cmd = { "lua-language-server" },
-        root_dir = vim.fs.root(0, { ".git", ".config" }),
-      })
-
-      vim.lsp.start({
-        name = "bashls",
-        cmd = { "bash-language-server", "start" },
-        root_dir = vim.fs.root(0, { ".git" }),
-      })
-
-      -- Common LSP keymaps
+      -- LSP keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
     end,
   },
+
+  -- nvim-cmp completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        },
+      })
+    end,
+  },
+
+  -- LuaSnip snippets
+  { "L3MON4D3/LuaSnip" },
 }
 
