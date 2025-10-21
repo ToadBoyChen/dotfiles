0f9cbbdd44db67b6a69745f131733dea7fbda7c1
toby <toby.chen1337@outlook.com>
2025-10-21 13:30:24 +0100

    use absolute path for launch script and add auto complete to nvim

--- hypr/scripts/launch/launch.sh
+++ hypr/scripts/launch/launch.sh
@@ -6,9 +6,9 @@ run_launch=1
 
 while [ "$run_launch" = 1 ];
 do
-    ./title-screen.sh
-    ./update-system.sh
-    ./clean-system.sh 
+    ./.config/hypr/scripts/launch/title-screen.sh
+    ./.config/hypr/scripts/launch/update-system.sh
+    ./.config/hypr/scripts/launch/clean-system.sh 
 
     run_launch=0
 done
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,47 +1,65 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { 
-                    "lua_ls",
-                    "pyright",
-                    "clangd",
-                    "ts_ls",
-                    "bashls",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
 
-            vim.lsp.config('lua_ls', {
-                cmd = { "lua-language-server" },
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = { globals = { "vim" } },
-                    },
-                },
-            })
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "clangd",
+          "ts_ls",
+          "bashls",
+        },
+      })
+    end,
+  },
 
-            vim.lsp.config('bashls', {
-                capabilities = capabilities,
-            })
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
-            vim.keymap.set("n", "<C-.>", vim.lsp.buf.definition, { desc = "Go to Definition" })
-            vim.keymap.set("n", "<C-a>", vim.lsp.buf.code_action, { desc = "Code Action" })
-        end,
-    },
+      -- Use the new Neovim 0.11+ API
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      })
+
+      vim.lsp.config("bashls", { capabilities = capabilities })
+      vim.lsp.config("pyright", { capabilities = capabilities })
+      vim.lsp.config("clangd", { capabilities = capabilities })
+      vim.lsp.config("ts_ls", { capabilities = capabilities })
+
+      -- Start configured LSPs automatically
+      vim.lsp.start({
+        name = "lua_ls",
+        cmd = { "lua-language-server" },
+        root_dir = vim.fs.root(0, { ".git", ".config" }),
+      })
+
+      vim.lsp.start({
+        name = "bashls",
+        cmd = { "bash-language-server", "start" },
+        root_dir = vim.fs.root(0, { ".git" }),
+      })
+
+      -- Common LSP keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+    end,
+  },
 }
 
