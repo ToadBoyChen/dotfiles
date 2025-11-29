e5f0572ef368160c544ecbf14f8ee3bf4db544e0
toby <toby.chen1337@outlook.com>
2025-11-29 18:04:40 +0000

    improve system a lil

--- nvim/lua/plugins/colour.lua
+++ nvim/lua/plugins/colour.lua
@@ -1,9 +1,79 @@
 return {
-    "scottmckendry/cyberdream.nvim",
+    "catppuccin/nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd("colorscheme cyberdream")
+        require("catppuccin").setup({
+            flavour = "auto",
+            background = {
+                light = "latte",
+                dark = "mocha",
+            },
+            transparent_background = false,
+            float = {
+                transparent = false, -- enable transparent floating windows
+                solid = false, 
+            },
+            show_end_of_buffer = false, 
+            term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+            dim_inactive = {
+                enabled = false, -- dims the background color of inactive window
+                shade = "dark",
+                percentage = 0.15
+            },
+            no_italic = false, -- Force no italic
+            no_bold = false, -- Force no bold
+            no_underline = false, -- Force no underline
+            styles = {
+                comments = { "italic" }, -- Change the style of comments
+                conditionals = { "italic" },
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+                -- miscs = {}, -- Uncomment to turn off hard-coded styles
+            },
+            lsp_styles = {
+                virtual_text = {
+                    errors = { "italic" },
+                    hints = { "italic" },
+                    warnings = { "italic" },
+                    information = { "italic" },
+                    ok = { "italic" },
+                },
+                underlines = {
+                    errors = { "underline" },
+                    hints = { "underline" },
+                    warnings = { "underline" },
+                    information = { "underline" },
+                    ok = { "underline" },
+                },
+                inlay_hints = {
+                    background = true,
+                },
+            },
+            color_overrides = {},
+            custom_highlights = {},
+            default_integrations = true,
+            auto_integrations = false,
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                neotree = true,
+                notify = false,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
+                },
+            },
+        })
+
+        vim.cmd.colorscheme "catppuccin"
     end
 }
-
