fcbfeaf70cf56828a9a7ed1b23118046fa63bd76
toby <toby.chen1337@outlook.com>
2025-12-31 12:10:50 +0000

    update for compatabilty with new hyprland

---
+++ nvim/lua/plugins/autotag.lua
@@ -0,0 +1,25 @@
+return {
+    "windwp/nvim-ts-autotag",
+    config = function()
+        require('nvim-ts-autotag').setup({
+            opts = {
+                -- Defaults
+                enable_close = true, -- Auto close tags
+                enable_rename = true, -- Auto rename pairs of tags
+                enable_close_on_slash = false -- Auto close on trailing </
+            },
+            -- Per filetype overrides
+            per_filetype = {
+                ["html"] = {
+                    enable_close = true
+                },
+                ["typescriptreact"] = {
+                    enable_close = true
+                },
+                ["javascriptreact"] = {
+                    enable_close = true
+                }
+            }
+        })
+    end,
+}
